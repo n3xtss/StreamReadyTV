93e03446c91435c40ee592275bea1f77ae46747b
n3xtss <cosminact97@gmail.com>
2026-09-22 19:16:17 +0100

    Prepare StreamReady v0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1064
-        versionName = "1.1.0-beta.1"
+        versionCode = 1
+        versionName = "0.1.0"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
